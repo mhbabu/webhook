f19f6171dd778b8d23d453cfdc891726fd7d91ca
mhbabu <write2babu.cse@gmail.com>
2025-07-16 13:27:32 +0600

    Facebook and WhatsApp webhook implementation done

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookController;
 
-Route::get('inbound/whatapp/message', [WebhookController::class, 'verify']); // GET for webhook verification
-Route::post('inbound/whatapp/message', [WebhookController::class, 'whatsapp']); // POST for message reception
+Route::get('webhook/whatapp', [WebhookController::class, 'verify']); // GET for webhook verification
+Route::post('webhook/whatapp', [WebhookController::class, 'whatsapp']); // POST for message reception
 
 Route::get('webhook/instagram', [WebhookController::class, 'verifyIntragram']); // For for webhook verification
 Route::post('webhook/instagram', [WebhookController::class, 'receiveInstragramMsg']); // POST for message reception
