27173acdf9fa4b729361bb0fbd2c3081dfbbcf65
mhbabu <write2babu.cse@gmail.com>
2025-08-26 17:04:43 +0600

    Customer Table and conversation table implementation

--- database/migrations/2025_07_14_073821_create_messages_table.php
+++ database/migrations/2025_07_14_073821_create_messages_table.php
@@ -21,6 +21,10 @@ return new class extends Migration
             $table->enum('direction', ['incoming', 'outgoing']);
             $table->timestamps();
         });
+
+        Schema::table('conversations', function (Blueprint $table) {
+            $table->foreignId('last_message_id')->nullable()->constrained('messages');
+        });
     }
 
     /**
