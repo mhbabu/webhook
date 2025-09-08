f2bfc83ee099feaed002274e7aafc094bb8627ba
mhbabu <write2babu.cse@gmail.com>
2025-09-08 16:43:35 +0600

    WebsocketIncomingMessage - method log defined

--- app/Http/Controllers/Api/Message/MessageController.php
+++ app/Http/Controllers/Api/Message/MessageController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers\Api\Message;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class MessageController extends Controller
 {
-    public function incomingMsg(Request $request){
-        
+    public function incomingMsg(Request $request)
+    {
+        $data = $request->all();
+
+        // Log the full request data as JSON
+        Log::info('Incoming Message Data:', $data);
+
+        // Or if you want it as a JSON string
+        Log::info('Incoming Message Data: ' . json_encode($data, JSON_PRETTY_PRINT));
+
+        return response()->json(['status' => 'success', 'data'   => $data]);
     }
 }
