5369e2027226763c6e612f489dfd462ba4c2fcb5
mhbabu <write2babu.cse@gmail.com>
2025-09-30 17:51:36 +0600

    handler logged

---
+++ app/Http/Resources/Message/ConversationInfoResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources\Message;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CustomerResource;
+use App\Http\Resources\Message\MessageResource;
+use App\Http\Resources\Message\WrapUpConversationResource as MessageWrapUpConversationResource;
+use App\Http\Resources\WrapUpConversationResource;
+
+class ConversationInfoResource extends JsonResource
+{
+    protected $message;
+
+    public function __construct($conversation, $message = null)
+    {
+        parent::__construct($conversation);
+        $this->message = $message;
+    }
+
+    public function toArray($request): array
+    {
+        $conversation = $this->resource;
+        $message = $this->message;
+
+        return [
+            'id'              => $conversation->id,
+            'platform'        => $conversation->platform,
+            'trace_id'        => $conversation->trace_id,
+            'customer'        => $conversation->customer ? new CustomerResource($conversation->customer) : null,
+
+            // Use the passed message here
+            'last_message'    => $message?->content ?? null,
+            'last_message_at' => $message?->created_at ? $message->created_at->toDateTimeString() : null,
+
+            'last_message_info' => $message ? new MessageResource($message) : null,
+
+            'started_at'      => $conversation->started_at,
+            'end_at'          => $conversation->end_at,
+            'wrap_up_info'    => $conversation->wrapUp ? new MessageWrapUpConversationResource($conversation->wrapUp) : null,
+            'is_ended'        => (bool) $conversation->end_at,
+        ];
+    }
+}
