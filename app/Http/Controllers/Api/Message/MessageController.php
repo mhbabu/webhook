f051bca254ba1f3168d2f498e7537fe6cb77f07e
mhbabu <write2babu.cse@gmail.com>
2025-10-12 12:25:04 +0600

    MessageController

--- app/Http/Controllers/Api/Message/MessageController.php
+++ app/Http/Controllers/Api/Message/MessageController.php
@@ -137,7 +137,7 @@ class MessageController extends Controller
             $message->save();
         }
 
-        // Log::info('[Message Data] Updated message', ['message' => $message, 'receiver_id' => $message->receiver_id, 'agentId' => $agentId]);
+        Log::info('[Message Data] Updated message', ['message' => $message, 'receiver_id' => $message->receiver_id, 'agentId' => $agentId]);
 
         // DB::commit();
 
