5cdf2b48b017252ade48415e960cc885446d71e9
mhbabu <write2babu.cse@gmail.com>
2025-12-03 11:57:51 +0600

    EndcChat done

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Console\Commands\CustomerInactivityChecker;
+use App\Console\Commands\EndChatAlertChecker;
 use App\Http\Middleware\Customer\ValidateCustomerToken;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Auth\AuthenticationException;
@@ -41,7 +42,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withSchedule(function (Schedule $schedule) {
         $schedule->command(CustomerInactivityChecker::class)->everyMinute();
-        $schedule->command(CustomerInactivityChecker::class)->everyMinute();
+        $schedule->command(EndChatAlertChecker::class)->everyMinute();
 
     })
 
