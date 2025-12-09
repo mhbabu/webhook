c7b11b7f89829254741ac9c2ef1712236895464c
mhbabu <write2babu.cse@gmail.com>
2025-12-09 14:58:00 +0600

    Report - ConversationReport

--- routes/report/report.php
+++ routes/report/report.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\Report\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('report')->group(function () { //->middleware('auth:sanctum')
+Route::prefix('report')->middleware(['auth:sanctum'])->group(function () { //->middleware('auth:sanctum')
     Route::get('/conversations', [ReportController::class, 'conversationReport']);
     Route::get('/conversations/{conversation}', [ReportController::class, 'conversationDetails']);
 });
