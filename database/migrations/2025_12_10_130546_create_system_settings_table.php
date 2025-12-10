6f1357ebd7529a5691f479b163d049a67f9e062a
mhbabu <write2babu.cse@gmail.com>
2025-12-10 13:06:09 +0600

    Setting helpers

--- database/migrations/2025_12_10_130546_create_system_settings_table.php
+++ database/migrations/2025_12_10_130546_create_system_settings_table.php
@@ -4,17 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('system_settings', function (Blueprint $table) {
             $table->id();
             $table->string('setting_key')->unique(); // e.g., whatsapp, facebook, instagram
-            $table->json('setting_value');           // store tokens, IDs, callback URLs, etc.
+            $table->json('setting_value');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('system_settings');
