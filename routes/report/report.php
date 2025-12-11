0e62989bea019f5b4269da534ba5277a612edc01
mhbabu <write2babu.cse@gmail.com>
2025-12-11 17:48:00 +0600

    Report api issue solved

--- routes/report/report.php
+++ routes/report/report.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('report')->middleware(['auth:sanctum', 'role:Super Admin,Admin,Supervisor'])->group(function () { //->middleware('auth:sanctum')
     Route::get('/conversations', [ReportController::class, 'conversationReport']);
-    Route::get('report/conversation/details', [ReportController::class, 'conversationDetails']);
+    Route::get('/conversation/details', [ReportController::class, 'conversationDetails']);
 });
