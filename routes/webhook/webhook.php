e80bfd035e9f4c5ba2a896cf5dc89fc61d7b0cf0
nobel1980 <nobel.amin@gmail.com>
2025-12-23 12:36:03 +0600

    Separate Instagram webhook

--- routes/webhook/webhook.php
+++ routes/webhook/webhook.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Webhook\FacebookWebhookController;
+use App\Http\Controllers\Api\Webhook\InstagramWebhookController;
 use App\Http\Controllers\Api\Webhook\PlatformWebhookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,8 +13,8 @@ Route::post('webhook/whatsapp', [PlatformWebhookController::class, 'incomingWhat
 Route::get('webhook/messenger', [PlatformWebhookController::class, 'verifyMessengerToken']); //  messenger token verification
 Route::post('webhook/messenger', [PlatformWebhookController::class, 'incomingMessengerMessage']); // receive messenger webhook response
 
-Route::get('webhook/instagram', [PlatformWebhookController::class, 'verifyInstagram']); // For for webhook verification
-Route::post('webhook/instagram', [PlatformWebhookController::class, 'receiveInstagramMessage']); // POST for message reception
+Route::get('webhook/instagram', [InstagramWebhookController::class, 'verifyInstagram']); // For for webhook verification
+Route::post('webhook/instagram', [InstagramWebhookController::class, 'receiveInstagramMessage']); // POST for message reception
 
 Route::get('webhook/facebook', [PlatformWebhookController::class, 'verifyFacebookPageToken']); //  facebook page token verification
 // Route::post('webhook/facebook', [PlatformWebhookController::class, 'receiveFacebookPageEventData']); // receive facebook page event data
