4ba14d85b382210bad4dda452430d68d54cf69f3
mhbabu <write2babu.cse@gmail.com>
2026-01-05 10:13:04 +0600

    migration issue solved

--- database/migrations/2025_11_19_145834_create_platform_accounts_table.php
+++ database/migrations/2025_11_19_145834_create_platform_accounts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('platform_accounts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('platform_id')->constrained()->cascadeOnDelete();
-            $table->string('platform_account_id')->index(); // page id, ig business id, org id
+            $table->string('platform_account_id')->nullable(); // page id, ig business id, org id
             $table->string('name')->nullable();
             $table->string('username')->nullable();
             $table->json('credentials')->nullable(); // tokens, refresh tokens
