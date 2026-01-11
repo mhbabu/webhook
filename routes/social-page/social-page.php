0d0fe4393eeb10ba9a5edad576607617f1f209b5
mhbabu <write2babu.cse@gmail.com>
2026-01-11 10:26:18 +0600

    social-page conversation done

--- routes/social-page/social-page.php
+++ routes/social-page/social-page.php
@@ -9,11 +9,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('social-pages/conversation', [FacebookPageController::class, 'getAgentConversationList']);
     Route::get('social-pages/conversation/{conversationId}', [FacebookPageController::class, 'conversationWisePostDetails']);
     Route::post('social-pages/comment-reply/{conversationId}', [FacebookPageController::class, 'replyAComment']);
-
-    // Route::get('facebook-posts', [FacebookPostMockController::class, 'index']);
-    // Route::get('social-pages/conversation', [ThreadController::class, 'agentThreadList']);
-    // Route::get('threads/{Conversation}', [ThreadController::class, 'getConversationWiseThread'])
-
 });
 
 Route::get('sync-social-post-data', [FacebookPagePostSyncController::class, 'syncSocialPostData']);
